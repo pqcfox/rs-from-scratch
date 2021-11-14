9f414e0102d5db088511351cc5cad241190d180e
Katherine Watson <katie@radicalsemiconductor.com>
2021-11-14 15:41:16 -0800

    Implement finite field multiplication

--- finite.py
+++ finite.py
@@ -1,32 +1,86 @@
-class FiniteField:
-    def __init__(self, p, k):
-        self.p = p
-        self.k = k
-        self.q = p**k
-
-    def __call__(self, val):
-        return FiniteFieldElt(self, val)
-
-    def __str__(self):
-        return f'GF({self.q})'
-
-    def __repr__(self):
-        return f'FiniteField({self.p}, {self.k})'
+K = 8
+MOD = [1, 0, 0, 0, 1, 1, 0, 1, 1]
 
 
-class FiniteFieldElt:
-    def __init__(self, val, field):
-        self.val = val % field.q
-        self.field = field
+class QRFiniteField:
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
 
     def __str__(self):
-        return f'{self.val}'
+        # get list of exponents, then monomials
+        exps = range(K-1, -1, -1)
+        monos = [f'x^{exp}' if exp != 0 else '1'
+                 for coeff, exp in zip(self.coeffs, exps)
+                 if coeff == 1]
+
+        # join together with plusses
+        poly = ' + '.join(monos)
+        return poly
 
     def __repr__(self):
-        return f'<value {self.val} in {self.field}>'
+        # add some context for repr
+        return f'<value {self} in field GF(2^{K})>'
+
+    @staticmethod
+    def xor(a, b):
+        # just xor each element
+        return [a_elt ^ b_elt for a_elt, b_elt in zip(a, b)]
+
+    def __add__(self, other):
+        # adding is just xoring
+        coeffs = xor(self.coeffs, other.coeffs)
+        return QRFiniteField(coeffs)
+
+    def __mul__(self, other):
+        # create a product with all zeros
+        prod = [0 for _ in range(2 * (K-1) + 1)]
+
+        # perform raw multiplication
+        exps = range(K-1, -1, -1)
+
+        # go through all self's terms
+        for exp, coeff in zip(exps, self.coeffs):
+            # if a term is nonzero
+            if coeff == 1:
+
+                # create a partial sum by shifting other's coeffs
+                part_sum = other.coeffs + exp * [0]
+                part_sum = (len(prod) - len(part_sum)) * [0] + part_sum
+
+                # xor it into the product
+                prod = self.xor(prod, part_sum)
+
+        # long division to reduce
+        while True:
+            # find the first nonzero coeff in prod
+            prod_exps = range(2 * (K-1), -1, -1)
+            prod_degree = max([exp for exp, coeff in zip(prod_exps, prod)
+                               if coeff == 1])
+
+            print(prod)
+            print(prod_exps)
+
+            # if we're fully reduced, break
+            if prod_degree < K:
+                break
+
+            # shift our mod to reduce out the top degree
+            shift_mod = MOD + [0] * (prod_degree - K)
+            shift_mod = (len(prod) - len(shift_mod)) * [0] + shift_mod
+
+            print(shift_mod)
+            print('----------')
+
+            # xor it out of the product
+            prod = self.xor(prod, shift_mod)
+
+        # chop off the leading bits (now zero) and return
+        prod = prod[-K:]
+        return QRFiniteField(prod)
 
 
 if __name__ == '__main__':
-    k = FiniteField(2, 3)
-    a = FiniteFieldElt(3, k)
-    print(repr(a))
+    a = QRFiniteField([0, 1, 0, 1, 0, 0, 1, 1])
+    b = QRFiniteField([1, 1, 0, 0, 1, 0, 1, 0])
+
+    print(repr(a * b))
